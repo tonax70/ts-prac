04e2d09bce71e7defd1ad4133547a6d3d4b833ed
Toantoanhacker <canhtoan9262022@gmail.com>
2026-01-15 10:26:20 +0700

    add readme, modified utils including variable name as string

--- concepts/_utils.ts
+++ concepts/_utils.ts
@@ -1,3 +1,3 @@
-export const printVarInfo = (a) => {
-    console.log(`type of ${{a}/*object shorthand*/} is ${typeof a} as ${a}`);
+export const printVarInfo = (a, name:string) => {
+    console.log(`type of ${name/*object shorthand*/} is ${typeof a} as ${a}`);
 }
---
+++ concepts/b_number.ts
@@ -0,0 +1,21 @@
+import { printVarInfo } from "./_utils";
+
+let decimal: number = 67;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+let float: number = 3.14159;
+
+decimal = 0b1010;
+
+printVarInfo(decimal, "decimal");
+printVarInfo(hex, "hex");
+printVarInfo(binary, "binary");
+printVarInfo(octal, "octal");
+printVarInfo(float, "float");
+console.log(`
+    // again the types cant be re-assigned to a different type\n
+    // the types number can be dec, hex, bin, oct or float\n
+    // important note: since all number sub-type is within the number type,\n
+    // we can assign a different umber type t the previous type
+    `)

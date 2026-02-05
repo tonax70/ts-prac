b74b896679bcf9799797701983fa07f3de9d1529
tonax70 <canhtoan9a@gmail.com>
2026-02-05 11:15:58 +0700

    post increment/decrement and pre increment/decrement difference

--- dom/index.js
+++ dom/index.js
@@ -4,6 +4,25 @@ const taw  = () =>  {
     alert('yes, next please');
     document.getElementById('yes').innerHTML = 'hello again';
 }
+    let c = 0;
+
+const count_up = () => {
+    document.querySelector('h2').innerHTML = c++;
+
+    // pre-increment : c++
+    // post-increment: ++c
+    // this highlight the difference between c++ and ++c
+    
+    // in c++ case, when clicking a button, it will set iner html to c first and when render complete, it increment
+    // so when click the button it will print 0 first
+
+    //in ++c casem when clicking the button ti immediately increment ++c and then render the c value on the UI
+    // so when click the button it correctly display 1 and not 0
+}
+const count_down = () => {
+    document.querySelector('h2').innerHTML = c--;
+}
+
 
 //a function is global code block that can reuse anytime you want
 //a method is a function within a class that can be use in that class

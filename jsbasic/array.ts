66348ec13c06bab15fa1b90d03b06ddbaa9ba948
tonax70 <canhtoan9a@gmail.com>
2026-02-10 00:04:13 +0700

    array  common methods reviewed

--- jsbasic/array.ts
+++ jsbasic/array.ts
@@ -1,3 +1,5 @@
+
+
 const chrt : string[] = ["SKS", "NKN", "SHR", "KEJ", "SRA"];
 
 //accessing
@@ -38,6 +40,7 @@ console.log(chesraw());
 const [s, n, sh, k, sr] = chrt;
 console.log(`character: ${s} - ${n} - ${sh} - ${k} - ${sr}`);
 
+//spread syntax
 const [s2, n2, ...restChar] = chrt;
 console.log(`char" ${s2} - ${n2} - ${JSON.stringify(restChar)}`);
 
@@ -64,3 +67,51 @@ console.log(JSON.stringify(chrt));
 phs = chrt.shift();
 console.log("Shift new len: " + phs);
 console.log(JSON.stringify(chrt)); 
+
+// indexof return the index of the targeted element, if not found, return -1
+console.log(`Index of SKS: ${chrt.indexOf("SKS")}`);
+console.log(`Index of Tonax: ${chrt.indexOf("Tonax")}`);
+
+// concat creating a new array by merging 2 or more array
+let secarr = ["Tonax"];
+let secarrE:any[] = [];
+let newMergeAr = chrt.concat(secarr);
+console.log("new array concatenated: " + JSON.stringify(newMergeAr));
+// notice that with concat always create a enw array and the array wont be equal to the old one
+console.log(secarr === secarr);
+let secarrEE = secarrE.concat(secarr);
+console.log("Secarr: " + JSON.stringify(secarr));
+console.log("SecarrEE: "+JSON.stringify(secarrEE));
+if (secarr != secarrEE) console.log("Secarr and SecarrEE is not equal because concat created new Array");
+
+const lg = () => console.log(JSON.stringify(chrt));
+//splice : add or remove element from any position within the array - WARNING: this will mutate the original array
+console.log("from index 0, dont delete anything, insert 2 elements 'i' and 'love'");
+chrt.splice(0, 0, "i", "love");
+lg();
+console.log("from index 3, dont delete anything, insert 2 elements '| other' and 'characters'");
+chrt.splice(3, 0, "| other", "characters");
+lg();
+console.log("delete 6 elements from index 3");
+chrt.splice(3, 6);
+lg();
+
+
+//include return true if an element exist within the arr and false if not
+console.log(chrt.includes("SKS"));
+
+//slice creates a shallow copy of an array specified by the start and the end of the array, it wont mutate the original array
+console.log(chrt.slice(1,3)); // will be >= start index and < end index + 1
+
+let str = "Tonax";
+//split method creates an array of characters from a string based on input regex
+let charstr: string[] = str.split("");
+console.log(charstr);
+
+// reverse method reversing an array
+let arrR = charstr.reverse();
+
+//join method join all array element with defined char
+console.log(arrR.join("-"));
+
+

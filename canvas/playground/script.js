59665098b9d40bd884fecd43d953485ea65537ac
tonax70 <canhtoan9a@gmail.com>
2026-02-10 16:29:01 +0700

    basic coordnates done

--- canvas/playground/script.js
+++ canvas/playground/script.js
@@ -1,16 +1,43 @@
 const canvas = document.getElementById("myCanvas");
+
+// make the canvas display the full width
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = '#fcb001';
+// the canvas y direction is also upside down like lin lovec 2d
 
 // draw a rectangle
 ctx.fillRect(0,0,150,75);
 
 
 // draw a line
-ctx.moveTo(38, 8);
-ctx.lineTo(1000, 1000);
+ctx.moveTo(150, 0);
+ctx.lineTo(300, 75);
 // use the ink method
+ctx.stroke();
+
+// draw a line
+ctx.moveTo(150, 75);
+ctx.lineTo(300, 75);
+// use the ink method
+ctx.stroke();
+
+// draw a line
+ctx.moveTo(300, 0);
+ctx.lineTo(300, 75);
+// use the ink method
+ctx.stroke();
+
+
+// draw a circle
+/*
+beginPath() - begins a path
+arc(x, y, r, startangle, endangle) - creates an arc/curve.
+To create a circle with arc(): Set startangle to 0 and endangle to 2*Math.PI.
+The x- and y-coordinates define the center of the circle. r defines the radius of the circle
+*/
+ctx.beginPath();
+ctx.arc(337.5, 37.5, 37.5, 0, 2*Math.PI);
 ctx.stroke();

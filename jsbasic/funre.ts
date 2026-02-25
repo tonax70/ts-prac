64b43e62932f0a445c48e6dfb4c332cbe35c0313
tonax70 <canhtoan9a@gmail.com>
2026-02-25 09:52:48 +0700

    tostring

--- jsbasic/funre.ts
+++ jsbasic/funre.ts
@@ -5,3 +5,22 @@ let str1 = String(str0); console.log(typeof str1);
 // because when we use it, js will temporary wrap the variable into an object String so we can use the methods of the string object like so
 console.log(str0.length);
 // note that because the primitive types doesn't contains addition methods so it will run faster and more efficient than the string object
+
+// toString method ===============================================================================
+let sks_age = 800;
+
+// convert a value to it's string representation, in different base from 2 to 36
+//binary
+console.log("binary: " + sks_age.toString(2));
+//decimal
+console.log("dec: " + sks_age.toString(10));
+//octal
+console.log("octal: " + sks_age.toString(8));
+//hexadecimal
+console.log("hex: " + sks_age.toString(16));
+// base 36
+console.log("binary: " + sks_age.toString(36));
+//array tostring
+console.log("array tostring: " + [1,2,3].toString());
+
+

2acd55001122cb13de12a7ac6276569660b12c9c
tonax70 <canhtoan9a@gmail.com>
2026-03-05 15:07:11 +0700

    higher order function

--- func/calbac.ts
+++ func/calbac.ts
@@ -16,3 +16,41 @@ console.log("value: " + n + " | index: " + idx + "| arr: " + arr)});
 
 
 console.log(sum + " " + numbers.toString());
+
+
+// functions are first class citizen in javascript unlike java
+/* can:
+store as available
+passed as argument of another function
+can be returned from a function
+can be stored in a data object
+*/
+//type 1: takes a function as argument
+function OOA (arr:number[], fn:(n:number) => number) { // operations on array
+
+    let result = [];
+    
+    for (let n of arr) {
+        result.push(fn(n));
+    }
+
+    return result;
+
+}
+
+// can abstract away the operation and changes between different code blocks
+function x2 (number:number) {
+    return number * 2;
+}
+
+console.log(OOA(numbers, x2));
+
+// type 2 : return a function
+function fahh (bars: string) {
+    return function (statement: string) {
+        console.log(bars + " " + statement);
+    }
+}
+
+const yap = fahh("SKS");
+yap("is the GOAT");

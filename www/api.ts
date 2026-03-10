06919bd3458acf3c7f5cf29bc77ec262d1191270
tonax70 <canhtoan9a@gmail.com>
2026-03-10 21:38:03 +0700

    api basics

---
+++ www/api.ts
@@ -0,0 +1,16 @@
+// api stands for application programming interface
+
+// built on a set of protocol rules so that multiple program can communicate with each others effectively
+//web api designed for web apps
+//browser api expose data to the browser, as a web dev we can manipulate this type of data using javascript
+// classified into 2 types which is "Browser APIs" and "Third-party APIs"
+// common browser api include the DOM api which we can use js to manipulate its elements 
+// the storage api which we can manage user's data sae locally on the device
+
+// can also work with other types of elements such as videos and audios
+// browser apis are helpful tools for building web apps
+//we an also use various of third party apis like google map api, time and weather forecast etc.
+
+//web apis are essential  tools for building web apps.
+
+
